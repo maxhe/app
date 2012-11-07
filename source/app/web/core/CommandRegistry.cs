c6ceb97721910c786ada3ea8ddaa80fd693d4fcf
Max He <he_shilin@hotmail.com>
2012-11-07 16:33:18 -0700

    Pushing new changes

--- source/app/web/core/CommandRegistry.cs
+++ source/app/web/core/CommandRegistry.cs
@@ -7,16 +7,20 @@ namespace app.web.core
   public class CommandRegistry : IFindCommands
   {
     IEnumerable<IProcessOneRequest> commands;
+    MissingCommandFactory_Behaviour missing_command;
 
-    public CommandRegistry(IEnumerable<IProcessOneRequest> commands)
+    public CommandRegistry(IEnumerable<IProcessOneRequest> commands, MissingCommandFactory_Behaviour missing_command)
     {
       this.commands = commands;
+      this.missing_command = missing_command;
     }
 
     public IProcessOneRequest get_the_command_that_can_run(IContainRequestDetails request)
     {
-      return commands.First(x => x.can_run(request));
-    }
-  }
 
+      var command_can_run = commands.FirstOrDefault(x => x.can_run(request)) ?? missing_command();
+      return command_can_run;
+    }
+
+  }
 }
